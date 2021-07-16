52bd43a9ec719ce07fdf97593a478521aa028cba
KIMHWANSEUNG <49117761+KIMHWANSEUNG@users.noreply.github.com>
2021-07-16 14:46:39 +0900

    FEAT: 이메일 Url과 이메일 체크 url을 추가한다

--- src/main/java/com/sadadream/controllers/UserController.java
+++ src/main/java/com/sadadream/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.sadadream.controllers;
 
+import com.sadadream.application.EmailService;
 import com.sadadream.application.UserService;
 import com.sadadream.domain.User;
 import com.sadadream.dto.UserModificationData;
 import com.sadadream.dto.UserRegistrationData;
 import com.sadadream.dto.UserResultData;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +16,15 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/users")
 @CrossOrigin
+@Slf4j
 public class UserController {
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    private final EmailService emailService;
+
+    public UserController(UserService userService,  EmailService emailService) {
         this.userService = userService;
+        this.emailService = emailService;
     }
 
     @PostMapping
@@ -49,5 +55,27 @@ public class UserController {
                 .email(user.getEmail())
                 .name(user.getName())
                 .build();
+    }
+
+
+
+    @PostMapping("/email") // 이메일 인증 코드 보내기
+    public String emailAuth(@RequestParam String email) throws Exception {
+        String email_code=emailService.sendSimpleMessage(email);
+        log.info(("email에서 이메일 코드는 : "+email_code));
+
+        return "";
+    }
+
+    @PostMapping("/emailcheck")//이메일 코드 확인하기
+    public boolean emailcheck(@RequestParam String email_code){
+
+        if(EmailService.ePw.equals(email_code)){
+            log.info("emailcheck에서 이메일 코드:"+EmailService.ePw);
+            return true;
+        }else{
+            log.info("잘못된 이메일 코드 입력");
+            return false;
+        }
     }
 }
